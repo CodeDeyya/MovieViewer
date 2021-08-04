bc7097ede41549d5f65424a293b2b2a0d1587172
CodeDeyya <CodeDeyya@gmail.com>
2021-08-04 12:40:11 +0530

    all done except release_date

--- src/Details.js
+++ src/Details.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
+
 export default function Details() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
   let { id } = useParams();
+  const [detail, setDetail] = useState([]);
+  useEffect(() => {
+    async function getDetails() {
+      const response = await fetch(`/api/list/${id}`);
+      const payload = await response.json();
+      setDetail(payload);
+    }
+    getDetails();
+  }, []);
   return (
     <div>
-      <h3>ID: {id}</h3>
+      <h1>{detail.title}</h1>
+      <h3>{detail.tagline}</h3>
+      <p>{detail.overview}</p>
+      <h5>Release Data: {detail.release_date}</h5>
+      <h5>Run Time: {detail.runtime} Minutes</h5>
+      <h5>Status: {detail.status}</h5>
+      <h5>Vote Average : {detail.vote_average} /10</h5>
+      <h5>Vote Count: {detail.vote_count}</h5>
     </div>
   );
 }
